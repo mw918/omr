896e56fe07d3c3f0c0b67daa34c698d1ef45018f
Michael Wang <mw918@hotmail.com>
2020-03-19 21:08:51 -0400

    Added a conditional to the previous print statement

--- port/unix/omrsysinfo.c
+++ port/unix/omrsysinfo.c
@@ -3305,6 +3305,9 @@ uint32_t
 omrsysinfo_set_limit(struct OMRPortLibrary *portLibrary, uint32_t resourceID, uint64_t limit)
 {
 	/*printf("The result of resourceID right now is %d and the value of limit is %d\n", resourceID, limit);*/
+	if (resourceID & limit)
+		printf("The result of resourceID right now is %d and the value of limit is %d\n", resourceID, limit);
+	uint32_t rc = 0;
 	uint32_t hardLimitRequested = OMRPORT_LIMIT_HARD == (resourceID & OMRPORT_LIMIT_HARD);
 	uint32_t resourceRequested = resourceID & ~OMRPORT_LIMIT_HARD;
 	struct rlimit lim = {0, 0};
