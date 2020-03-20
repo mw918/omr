f967722c50739f870d390e9e855a5172fedf8ff9
Michael Wang <mw918@hotmail.com>
2020-03-20 15:05:03 -0400

    Put one final line right before Trc_PRT_sysinfo_set_limit_Exit

--- port/unix/omrsysinfo.c
+++ port/unix/omrsysinfo.c
@@ -3415,7 +3415,7 @@ printf("Right in the first AIXPPC, the value of rc is %d\n", rc);
 			rc = OMRPORT_LIMIT_UNKNOWN;
 		}
 	}
-
+	printf("The line before last, the value of rc is %d\n", rc);
 	Trc_PRT_sysinfo_set_limit_Exit(rc);
 	printf("Right before returning, the value of rc is %d\n", rc);
 	return rc;
